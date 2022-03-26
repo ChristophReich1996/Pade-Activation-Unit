c438a2be1d707faf3f65218e86b80ea4a747a063
Christoph Reich <34400551+ChristophReich1996@users.noreply.github.com>
2022-03-26 17:06:53 +0100

    Plot initial shapes and print parameters

--- example.py
+++ example.py
@@ -1,4 +1,5 @@
 import torch
+import matplotlib.pyplot as plt
 
 from pau import PAU, freeze_pau
 
@@ -13,7 +14,20 @@ def main() -> None:
     # Calc gradients
     output.sum().backward()
     # Freeze PAU weights
+    print(list(pau.parameters()))
+    print(pau.state_dict())
     pau = freeze_pau(pau)
+    print(list(pau.parameters()))
+    print(pau.state_dict())
+    # Plot PAUs for different initializations
+    x = torch.linspace(-4, 4, 1000)
+    for shape in ["relu", "leaky_relu_0_01", "leaky_relu_0_2", "leaky_relu_0_25", "leaky_relu_0_3",
+                  "leaky_relu_m0_5", "tanh", "swish", "sigmoid"]:
+        pau: PAU = PAU(initial_shape=shape)
+        y = pau(x)
+        plt.plot(x, y)
+        plt.title(shape)
+        plt.show()
 
 
 if __name__ == '__main__':
